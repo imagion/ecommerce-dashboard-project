c346db94015e0bc281570e482ff5ef76c3006f19
Imagion <algernoncowperwood@gmail.com>
2023-12-08 11:00:27 +0300

    added clerk auth

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 import { Button } from './components/ui/button';
+import { UserButton } from '@clerk/nextjs';
 
 export default function Home() {
   return (
     <div>
       <h1>Dashboard</h1>
       <Button variant='secondary'>Click me!</Button>
+      <UserButton afterSignOutUrl='/' />
     </div>
   );
 }
